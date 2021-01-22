11141312882ab17632ee56af4f5e4e8cb1e3d955
Roy <r.touw@jool.nl>
2021-01-22 11:21:24 +0100

    initial commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('connected');   
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+        console.log(msg)
+    });
+});
+
+server.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
